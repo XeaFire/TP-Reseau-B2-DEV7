cc5fd562cd34165c5ac3c950d7b9f2172ccfcbc4
DTristan <tristan.diard@ynov.com>
2024-11-21 12:00:06 +0100

    ben

---
+++ ws_i_3_server.py
@@ -0,0 +1,20 @@
+# from https://websockets.readthedocs.io/en/stable/howto/quickstart.html
+import asyncio
+import websockets
+
+async def hello(websocket):
+    print(websocket)
+    msg = await websocket.recv()
+    print(f"{msg}")
+    greeting = f"Hello client ! Received {msg}!"
+    await websocket.send(greeting)
+    print(f"{greeting}")
+
+
+async def main():
+    print("Server Started")
+    async with websockets.serve(hello, "5.5.5.11", 8765):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())

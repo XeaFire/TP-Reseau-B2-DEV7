4513b587041398b5fe355bc13eaf775c14f39af1
DTristan <tristan.diard@ynov.com>
2024-11-21 11:41:58 +0100

    ben

---
+++ ws_i_2_client.js
@@ -0,0 +1,19 @@
+function hello() {
+    const uri = "ws://5.5.5.11:8765";
+    const websks = new WebSocket(uri);
+    const userInput = prompt("What's your name?")
+
+    websks.onopen = () => {
+        websks.send(userInput);
+    };
+
+    websks.onmessage = (event) => {
+        console.log(event.data);
+        alert(event.data)
+        websks.close();
+    };
+}
+
+
+hello()
+

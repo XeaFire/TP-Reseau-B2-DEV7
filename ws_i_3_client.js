303d53e2f58d03502d49cfa78fb2ddcfe056ec92
DTristan <tristan.diard@ynov.com>
2024-11-22 10:12:58 +0100

    ben

--- ws_i_3_client.js
+++ ws_i_3_client.js
@@ -1,8 +1,7 @@
 function hello() {
     const uri = "ws://5.5.5.11:8765";
     const websks = new WebSocket(uri);
-    let userInput = prompt("Choose your username")
-
+    const userInput = prompt("What's your name?")
 
     websks.onopen = () => {
         websks.send(userInput);
@@ -10,15 +9,13 @@ function hello() {
 
     websks.onmessage = (event) => {
         console.log(event.data);
+        alert(event.data)
     };
 
-    userInput.onchange
-
     while (true) {
-        userInput = prompt("Chatbox : ")
+        let userInput = prompt("Chatbox : ")
         websks.send(userInput);
     }
-
 }
 
 
